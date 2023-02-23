1341fdd18d83ab08d0a39e3c36bf0dd150075861
leodarkseid <leodarkseid@gmail.com>
2023-02-23 23:11:56 +0100

    update

--- index14.c
+++ index14.c
@@ -11,4 +11,4 @@ printf("storage size for char data type:%d\n",sizeof(b));
 printf("Storage size of float data type:%d\n",sizeof(c)); 
 printf("Storage size for double data type:%d\n",sizeof(d));
 return 0;
-} 
+}
